8fe2add793bea362f1cd182cf4cf64ecec81f6ad
Carsten Arnholm <arnholm@arnholm.org>
2020-11-15 14:47:00 +0100

    Version 1.5-00

--- docs/search/classes_0.js
+++ docs/search/classes_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['array',['array',['../classarray.html',1,'']]],
+  ['as_5fargs',['as_args',['../classas__args.html',1,'']]],
   ['as_5fistream',['as_istream',['../classas__istream.html',1,'']]],
   ['as_5fostream',['as_ostream',['../classas__ostream.html',1,'']]]
 ];
--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -1,6 +1,11 @@
 var searchData=
 [
+  ['get_5fdouble',['get_double',['../classas__args.html#a48cf3e73ce4050809ca48de9da25de3a',1,'as_args::get_double(string arg) const'],['../classas__args.html#a58502b23b372a266f1c2bbd4d3fe6b67',1,'as_args::get_double(string arg, double def) const']]],
+  ['get_5fint',['get_int',['../classas__args.html#a9b22f06e1d87c2d3ef6540af38065607',1,'as_args::get_int(string arg) const'],['../classas__args.html#a68df3b0d5562672d561471ecfa4f720f',1,'as_args::get_int(string arg, int def) const']]],
   ['get_5fkey',['get_key',['../classmap__iterator.html#a129d5962c078f6cd4a1a5854e16576ab',1,'map_iterator']]],
   ['get_5fopindex',['get_opIndex',['../classmap.html#a2b824e9af923859c82bee8cc0da2c938',1,'map']]],
+  ['get_5fstring',['get_string',['../classas__args.html#a28dbde940e55ca8430cd422e5e856024',1,'as_args::get_string(string arg) const'],['../classas__args.html#a3a9bfff31afd1cba16e21e2736381608',1,'as_args::get_string(string arg, string def) const']]],
+  ['get_5fuint',['get_uint',['../classas__args.html#a8813aa20ef4d22e137f4264bc526f25c',1,'as_args::get_uint(string arg) const'],['../classas__args.html#ab0e5c6b6ebc6d39182ddfa49f0e97101',1,'as_args::get_uint(string arg, uint def) const']]],
+  ['get_5funsigned_5fdouble',['get_unsigned_double',['../classas__args.html#a463995f1a8b24f510ba4b6524aeb9791',1,'as_args::get_unsigned_double(string arg) const'],['../classas__args.html#a3e10362badf59a88632e02439ec3fe6e',1,'as_args::get_unsigned_double(string arg, double def) const']]],
   ['get_5fvalue',['get_value',['../classmap__iterator.html#a80852d9724f26ee2652ebd46e52c4f08',1,'map_iterator']]]
 ];
